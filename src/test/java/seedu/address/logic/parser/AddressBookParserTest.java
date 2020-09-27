6f8511f2a718aab9bee7db2d1280b3ac13ec9aa6
Aloysius Lim <aloysiuslim.dw@gmail.com>
2020-09-27 17:13:33 +0800

    Remove Address Field

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -58,7 +58,7 @@ public class AddressBookParserTest {
         Person person = new PersonBuilder().build();
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
         EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
-                + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
+                + INDEX_FIRST_PERSON.getOneBased());
         assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
     }
 
